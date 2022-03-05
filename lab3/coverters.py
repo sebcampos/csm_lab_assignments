b68ace74a619e9480425f4cb56ff216e9546d3e6
sebcampos <65099525+sebcampos@users.noreply.github.com>
2022-03-05 13:37:47 -0800

    Create coverters.py

---
+++ lab3/coverters.py
@@ -0,0 +1,18 @@
+#Chinese dollars, Mexican dollars, British dollars, Canadian dollars, Russian dollars, Spanish dollars
+def chinese_to_american(value: float) -> float:
+  return 
+
+def mexican_to_american(value: float) -> float:
+  return 
+
+def british_to_american(value: float) -> float:
+  return 
+
+def canadian_to_american(value: float) -> float:
+  return 
+
+def russian_to_american(value: float) -> float:
+  return 
+
+def spanish_to_american(value: float) -> float:
+  return 
