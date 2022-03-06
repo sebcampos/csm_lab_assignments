120fd5cdb7bf048f5253aebd8747c3cec445a0c7
sebash <sebcampos@criptext.com>
2022-03-05 20:40:33 -0800

    added converters.py file containing a Converter Class that inits with hardcoded conversion rates taken from https://www.xe.com/currencyconverter and a method `convert` which converts a provided value with a specified currency to american dollars or performs the reverse operation if the reverse boolean flag is set to True.

--- lab3/coverters.py
+++ lab3/coverters.py
@@ -1,18 +1,39 @@
-#Chinese dollars, Mexican dollars, British dollars, Canadian dollars, Russian dollars, Spanish dollars
-def chinese_to_american(value: float) -> float:
-  return 
+# Chinese dollars, Mexican dollars, British dollars, Canadian dollars, Russian dollars, Spanish dollars
 
-def mexican_to_american(value: float) -> float:
-  return 
+# conversions taken from https://www.xe.com/currencyconverter/convert/
 
-def british_to_american(value: float) -> float:
-  return 
+class Converter:
+    def __init__(self) -> None:
+        """
+        Converter Class initialized with conversion rates saved as attribute coversion values
+        Attributes:
+          :conversion_values dictionary which
+           defines the US dollar amount of 1 unit of the corresponding currency
+        """
+        self.conversion_values = {
+          "Chinese Yuan Renminbi": 0.158252,
+          "Mexican Peso": 0.0477181,
+          "British Pound": 1.32300,
+          "Canadian Dollar": 0.785367,
+          "Russian Rubles": 0.00816314,
+          "Spanish Euro": 1.09431
+        }
 
-def canadian_to_american(value: float) -> float:
-  return 
+    def convert(self, currency: str, value: float, reverse: bool = False) -> float:
+        """
+        This method performs the conversion of one currency `currency` to american dollars, the conversion
+        occurs on the `value` provided then is returned. If the `reverse` bool is set to True then the conversion
+        from american dollars to the specified `currency` for the specified `value` is returned
+        :param currency: currency to be converted as a string
+        :param value: value to be converted as a float
+        :param reverse: boolean for reverse operation default set to False
+        :return: float of conversion representation
+        """
+        if reverse:
+            conversion = value / self.conversion_values[currency]
+            print(f"{value} american dollars equals {currency}:{conversion}")
+            return conversion
+        conversion = value * self.conversion_values[currency]
+        print(f"{currency}:{value} equals {conversion} american dollars")
+        return conversion
 
-def russian_to_american(value: float) -> float:
-  return 
-
-def spanish_to_american(value: float) -> float:
-  return 
