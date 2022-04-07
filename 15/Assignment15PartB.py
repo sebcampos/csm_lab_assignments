17ccd7a4786f17e569ee40f120a62da7505ae50d
sebash <sebcampos@criptext.com>
2022-04-07 13:20:32 -0700

    Adding a class to Assignment 15 part B to manage tkinter widgets

--- 15/Assignment15PartB.py
+++ 15/Assignment15PartB.py
@@ -3,34 +3,60 @@
 # 2022-03-28
 
 import tkinter as tk
-import datetime
-import os
+import turtle as t
+import re
 from Assignment15PartA import main as email_scraper
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.geometry("300x150")
+        self.geometry("750x250")
         self.title("Email Scraper")
+        self.frame_one = tk.Frame(
+            self,
+            bg="yellow",
+            bd=12
+        )
+        self.entry = tk.Entry(
+            self.frame_one
+        )
+
+        self.btn = tk.Button(
+            self,
+            text="Collect emails",
+            command=lambda entry=self.entry: self.scrape(entry)
+        )
+
+    def clean_url(self, url: str) -> str:
+        """
+
+        :param url:
+        :return:
+        """
+        if "https://" not in url:
+            url = "https://" + url
+
+        if re.search(r"https://.+\.[a-zA-Z]{3}", url):
+            return url
+        self.raise_invalid_url_popup(url)
 
     def scrape(self, entry):
-        url = entry.get()
-        print(url)
+        url = self.clean_url(entry.get())
         emails = email_scraper(url)
         print(emails)
 
-    def build(self):
-        frame = tk.Frame(self, bg="yellow", bd=12)
-        frame.pack()
-        entry = tk.Entry(frame)
-        entry.pack()
-        btn = tk.Button(
-            self,
-            text="Collect emails",
-            command=lambda txt=entry: self.scrape(entry)
-        )
-        btn.pack()
+    def raise_invalid_url_popup(self, url: str) -> None:
+        pop_up = tk.Toplevel(self)
+        pop_up.geometry("300x150")
+        pop_up.title("Child Window")
+        tk.Label(pop_up, text="Hello World!", font=('Mistral 18 bold')).place(x=150, y=80)
+        pass
+
+    def build(self: tk.Tk) -> tk.Tk:
+        self.entry.pack()
+        self.frame_one.pack()
+        self.btn.pack()
         return self
 
 
